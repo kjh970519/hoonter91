c31794e4a33ad22c818e16227b31bf23f192e750
jinhyeok970519 <kjh970519_@naver.com>
2026-01-28 23:32:11 +0900

    db 정보 변경

--- www/data/dbconn.set.php
+++ www/data/dbconn.set.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_ENGINE', 'mysql');
-define('DB_HOST', 'mysql56_db');
+define('DB_HOST', '49.50.129.138');
 define('DB_USER', 'hoonter91');
 define('DB_PWD', 'hoon45789');
 define('DB_NAME', 'hoonter91');
